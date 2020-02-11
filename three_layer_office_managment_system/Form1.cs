0c14ccf447e54a5862634db74741993d48cf588c
ratroot92 <zcomisb.007@gmail.com>
2020-02-11 11:34:18 +0500

    asd

---
+++ three_layer_office_managment_system/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace three_layer_office_managment_system
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
