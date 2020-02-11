a83bab799b0b7d8b9f339e903dbe9b148e000739
ratroot92 <zcomisb.007@gmail.com>
2020-02-11 13:29:13 +0500

    sd

---
+++ DataLayer/EmpDAL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using prjProps;
+namespace DataLayer
+{
+    public class EmpDAL
+    {
+        
+        public bool emp_insertDAL(EmpProps p)
+        {
+
+
+            MySqlCommmand cmd = new MySqlCommmand();
+            string sql="INSERT INTO emp (emp,name,code,address), values ()"
+            return true;
+        }
+    }
+}
