4af336571b2ce35cd8a00e4fc1f1c15fa5af4f44
ratroot92 <zcomisb.007@gmail.com>
2020-02-12 15:50:10 +0500

    asd

---
+++ DataLayer/connectionSQL.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataLayer
+{
+    class connectionSQL
+    {
+       public   string conn_string = '';
+
+        SqlConnection conn;
+
+        public SqlConnection db_start(string conn_string)
+        {
+            conn = new SqlConnection(conn_string);
+            conn.Open();
+            Console.WriteLine("connection started at ");
+            return conn;
+        }
+        public bool db_end()
+        {
+            conn.Close();
+            return true; 
+        }
+
+        public bool UDI(string qry, string conn_string)
+        {
+           conn=db_start(conn_string);
+            SqlCommand cmd = new SqlCommand(qry, conn);
+            int rows_affected = cmd.ExecuteNonQuery();\
+            if (rows_affected>0)
+            {
+                return true;
+            }
+            else
+            {
+                return  false;
+            }
+
+
+        }
+
+
+        public DataTable getDatatable(string qry,conn)
+        {
+
+        }
+      
+
+    }
+}
---
+++ DataLayer/connedctionMYSQL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class connedctionMYSQL
+    {
+        private String con_String = '';
+        private String username = 'root';
+        private String password = '';
+       
+       
+
+
+    }
+}
